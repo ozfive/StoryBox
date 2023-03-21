9659a923cfa69c58df2bf2654813728f1b67dea5
Chris <straight.chris@gmail.com>
2023-03-20 20:32:02 -0700

    Adding more commands to install.sh to move the storybox service file to the appropriate place along with moving the started.mp3 file to /etc/sound/. This commit also adds logging to a file based on XDG_DATA_HOME in the startup main.go file so that if the audio isn't played we have a record of it in a log file.

--- Startup/main.go
+++ Startup/main.go
@@ -1,28 +1,36 @@
 package startup
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
-func playStartUpSound() {
-
-	cmd := "mpg123-alsa"
-
-	// Final location: /etc/sound/started.mp3
-	startupSoundFile := "/home/pi/go/src/Storybox/Startup/started.mp3"
-
-	args := []string{startupSoundFile}
-	if err := exec.Command(cmd, args...).Run(); err != nil {
-
-		log.Println(os.Stderr, err)
-		os.Exit(1)
-	}
+func main() {
+	playStartUpSound()
 }
 
-func main() {
+func playStartUpSound() {
+	cmd := "mpg123-alsa"
 
-	playStartUpSound()
+	startupSoundFile := "/etc/sound/started.mp3"
 
+	args := []string{startupSoundFile}
+
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		logFile, err := os.OpenFile(filepath.Join(os.Getenv("XDG_DATA_HOME"), "storybox", "logs", "startup-errors.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println("Failed to open log file:", err)
+			os.Exit(1)
+		}
+
+		defer logFile.Close()
+
+		logger := log.New(logFile, "", log.LstdFlags)
+		logger.Println(fmt.Errorf("failed to play startup sound: %v", err))
+
+		os.Exit(1)
+	}
 }
